d1661ff1fcafb17bb1a5f6158fc4733b1ba6c16d
Sean Metcalf <sean@seanmart.com>
2022-01-13 23:46:28 -0500

    testing weather workflow

--- .github/actions/weather/index.js
+++ .github/actions/weather/index.js
@@ -1,11 +1,14 @@
-const core = require('@actions/core');
+import {getInput} from "@actions/core"
+import * as prismic from "@prismicio/client"
 
 async function getWeather(){
-  const PRISMIC_ACCESS_TOKEN = core.getInput('PRISMIC_ACCESS_TOKEN')
-  const PRISMIC_END_POINT = core.getInput('PRISMIC_END_POINT')
-  const WEATHER_ACCESS_TOKEN = core.getInput('WEATHER_ACCESS_TOKEN')
+  const PRISMIC_ACCESS_TOKEN = getInput('PRISMIC_ACCESS_TOKEN')
+  const PRISMIC_END_POINT = getInput('PRISMIC_END_POINT')
+  const WEATHER_ACCESS_TOKEN = getInput('WEATHER_ACCESS_TOKEN')
 
-  console.log(!!PRISMIC_ACCESS_TOKEN,!!PRISMIC_END_POINT,!!WEATHER_ACCESS_TOKEN)
+  let client = prismic.createClient(PRISMIC_END_POINT,{accessToken:PRISMIC_ACCESS_TOKEN})
+  let results = await client.getAllByType("event")
+  console.log(results)
 }
 
 getWeather()
