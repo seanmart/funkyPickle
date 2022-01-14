f624600e54cba7c5adf7d109109f244aead510b6
Sean Metcalf <sean@seanmart.com>
2022-01-14 00:54:35 -0500

    testing weather workflow

--- .github/actions/weather/dist/index.js
+++ .github/actions/weather/dist/index.js
@@ -9386,42 +9386,46 @@ function fixResponseChunkedTransferBadEnding(request, errorCallback) {
 
 
 async function getWeather(){
+
   const PRISMIC_ACCESS_TOKEN = (0,core.getInput)('PRISMIC_ACCESS_TOKEN')
   const PRISMIC_END_POINT = (0,core.getInput)('PRISMIC_END_POINT')
   const WEATHER_ACCESS_TOKEN = (0,core.getInput)('WEATHER_ACCESS_TOKEN')
 
-  let client = createClient(PRISMIC_END_POINT,{
-    accessToken:PRISMIC_ACCESS_TOKEN,
-    fetch: async (url,options)=>{
-      const res = await fetch(url,options)
-      if (res.ok) return res
-    }
-  })
+  let client = getPrismicClient(PRISMIC_END_POINT,PRISMIC_ACCESS_TOKEN)
   let events = await client.getAllByType("event")
 
   if (events){
 
-    const getData = async (map)=>{
-      let lon = map.longitude
-      let lat = map.latitude
-      let exclude = 'minutely,hourly,daily,alerts'
-
-      if(lon && lat){
-        let query = `lat=${lat}&lon=${lon}&exclude=${exclude}&appid=${WEATHER_ACCESS_TOKEN}`
-        let res = await fetch(`https://api.openweathermap.org/data/2.5/onecall?${query}`)
-        if (res.ok){
-          return res.json()
-        }
-      }
-    }
-
     for(const event of events){
-      getData(event.data.map)
+      getWeatherData(event.data.map,WEATHER_ACCESS_TOKEN)
       .then(data =>{
         console.log(event.data.title)
         console.log(data)
       })
     }
+  }
+}
+
+function getPrismicClient(endPoint,accessToken){
+  return createClient(endPoint,{
+    accessToken,
+    fetch: async (url,options)=>{
+      const res = await fetch(url,options)
+      if (res.ok) return res
+    }
+  })
+}
+
+async function getWeatherData(coordinates,accessToken){
+  let lon = coordinates.longitude
+  let lat = coordinates.latitude
+  let exclude = 'minutely,hourly,daily,alerts'
+
+  if(lon && lat){
+    let query = `lat=${lat}&lon=${lon}&exclude=${exclude}&appid=${accessToken}`
+    let res = await fetch(`https://api.openweathermap.org/data/2.5/onecall?${query}`)
+    if (res.ok) return res.json()
+    return {}
   }
 }
 
--- .github/actions/weather/index.js
+++ .github/actions/weather/index.js
@@ -3,42 +3,46 @@ import * as prismic from "@prismicio/client"
 import fetch from "node-fetch";
 
 async function getWeather(){
+
   const PRISMIC_ACCESS_TOKEN = getInput('PRISMIC_ACCESS_TOKEN')
   const PRISMIC_END_POINT = getInput('PRISMIC_END_POINT')
   const WEATHER_ACCESS_TOKEN = getInput('WEATHER_ACCESS_TOKEN')
 
-  let client = prismic.createClient(PRISMIC_END_POINT,{
-    accessToken:PRISMIC_ACCESS_TOKEN,
-    fetch: async (url,options)=>{
-      const res = await fetch(url,options)
-      if (res.ok) return res
-    }
-  })
+  let client = getPrismicClient(PRISMIC_END_POINT,PRISMIC_ACCESS_TOKEN)
   let events = await client.getAllByType("event")
 
   if (events){
 
-    const getData = async (map)=>{
-      let lon = map.longitude
-      let lat = map.latitude
-      let exclude = 'minutely,hourly,daily,alerts'
-
-      if(lon && lat){
-        let query = `lat=${lat}&lon=${lon}&exclude=${exclude}&appid=${WEATHER_ACCESS_TOKEN}`
-        let res = await fetch(`https://api.openweathermap.org/data/2.5/onecall?${query}`)
-        if (res.ok){
-          return res.json()
-        }
-      }
-    }
-
     for(const event of events){
-      getData(event.data.map)
+      getWeatherData(event.data.map,WEATHER_ACCESS_TOKEN)
       .then(data =>{
         console.log(event.data.title)
         console.log(data)
       })
     }
+  }
+}
+
+function getPrismicClient(endPoint,accessToken){
+  return prismic.createClient(endPoint,{
+    accessToken,
+    fetch: async (url,options)=>{
+      const res = await fetch(url,options)
+      if (res.ok) return res
+    }
+  })
+}
+
+async function getWeatherData(coordinates,accessToken){
+  let lon = coordinates.longitude
+  let lat = coordinates.latitude
+  let exclude = 'minutely,hourly,daily,alerts'
+
+  if(lon && lat){
+    let query = `lat=${lat}&lon=${lon}&exclude=${exclude}&appid=${accessToken}`
+    let res = await fetch(`https://api.openweathermap.org/data/2.5/onecall?${query}`)
+    if (res.ok) return res.json()
+    return {}
   }
 }
 
