b0c46444df7d705290861ad5cb97231dd5e4456b
Sean Metcalf <sean@seanmart.com>
2022-01-13 23:13:08 -0500

    testing weather workflow

--- .github/actions/weather/index.js
+++ .github/actions/weather/index.js
@@ -1,9 +1,9 @@
 const core = require('@actions/core');
 
 async function getWeather(){
-  const PRISMIC_ACCESS_TOKEN = JSON.parse(getInput('PRISMIC_ACCESS_TOKEN'))
-  const PRISMIC_END_POINT = JSON.parse(getInput('PRISMIC_END_POINT'))
-  const WEATHER_ACCESS_TOKEN = JSON.parse(getInput('WEATHER_ACCESS_TOKEN'))
+  const PRISMIC_ACCESS_TOKEN = core.getInput('PRISMIC_ACCESS_TOKEN'))
+  const PRISMIC_END_POINT = core.getInput('PRISMIC_END_POINT'))
+  const WEATHER_ACCESS_TOKEN = core.getInput('WEATHER_ACCESS_TOKEN'))
 
   console.log(!!PRISMIC_ACCESS_TOKEN,!!PRISMIC_END_POINT,!!WEATHER_ACCESS_TOKEN)
 }
