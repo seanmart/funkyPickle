e1c92e909463b372c693e7c9be0eaae11635df03
Sean Metcalf <sean@seanmart.com>
2022-01-14 01:01:07 -0500

    testing weather workflow

--- .github/actions/weather/dist/index.js
+++ .github/actions/weather/dist/index.js
@@ -9393,16 +9393,14 @@ async function getWeather(){
 
   let client = getPrismicClient(PRISMIC_END_POINT,PRISMIC_ACCESS_TOKEN)
   let events = await client.getAllByType("event")
+  let doc = {}
 
   if (events){
-
     for(const event of events){
-      getWeatherData(event.data.map,WEATHER_ACCESS_TOKEN)
-      .then(data =>{
-        console.log(event.data.title)
-        console.log(data)
-      })
+      let data = getWeatherData(event.data.map,WEATHER_ACCESS_TOKEN)
+      doc[event.uid] = data
     }
+    console.log(doc)
   }
 }
 
@@ -9424,9 +9422,12 @@ async function getWeatherData(coordinates,accessToken){
   if(lon && lat){
     let query = `lat=${lat}&lon=${lon}&exclude=${exclude}&appid=${accessToken}`
     let res = await fetch(`https://api.openweathermap.org/data/2.5/onecall?${query}`)
-    if (res.ok) return res.json()
-    return {}
+    if (res.ok){
+      let data = await res.json()
+      return data
+    }
   }
+  return {}
 }
 
 getWeather()
