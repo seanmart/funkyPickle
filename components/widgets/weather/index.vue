e8e29caeb7f620fce57e000669e3ae073432c449
Sean Metcalf <sean@seanmart.com>
2022-01-17 00:30:39 -0500

    updated weather workflow

--- components/widgets/weather/index.vue
+++ components/widgets/weather/index.vue
@@ -1,127 +1,121 @@
 <template lang="html">
-  <widget class="c-weather-widget" :hide="!current">
+  <widget class="c-weather-widget" :hide="!data">
     <template #content>
-      <template v-if="current">
-
+      <template v-if="data">
         <div class="c-icon--wrapper">
-          <weather-icon :icon="current.weather[0].icon"/>
+          <weather-icon :icon="data.weather[0].icon" />
         </div>
 
         <div class="c-temp--wrapper">
-          <h3 class="c-temp-value t-header" v-html="getTemp(current.temp)"/>
-          <span class="c-temp-description" v-html="current.weather[0].main"/>
+          <h3 class="c-temp-value t-header" v-html="getTemp(data.temp)" />
+          <span class="c-temp-description" v-html="data.weather[0].main" />
         </div>
 
-        <div class="c-bar"/>
+        <div class="c-bar" />
 
         <div class="c-info--wrapper">
           <table class="c-info-table">
-            <template v-for="(item,i) in info">
+            <template v-for="(item, i) in info">
               <tr class="c-info-row">
-                <template v-if="current[item.key]">
-                  <td class="c-info-label" v-html="`${item.label}:`"/>
-                  <td v-if="item.temp" v-html="getTemp(current[item.key])"/>
-                  <td v-else-if="item.time" v-html="getTime(current[item.key])"/>
-                  <td v-else-if="item.percent" v-html="current[item.key] + '%'"/>
+                <template v-if="data[item.key]">
+                  <td class="c-info-label" v-html="`${item.label}:`" />
+                  <td v-if="item.temp" v-html="getTemp(data[item.key])" />
+                  <td v-else-if="item.time" v-html="getTime(data[item.key])" />
+                  <td v-else-if="item.percent" v-html="data[item.key] + '%'" />
                 </template>
               </tr>
             </template>
           </table>
         </div>
-
       </template>
     </template>
   </widget>
 </template>
 
 <script>
-import weatherIcon from './weatherIcon'
-import widget from '../widget'
-import {mapState} from 'vuex'
+import weatherIcon from "./weatherIcon";
+import widget from "../widget";
+import { mapState } from "vuex";
 
 export default {
-  name: 'WeatherWidget',
+  name: "WeatherWidget",
   props: ["uid"],
-  components:{weatherIcon,widget},
+  components: { weatherIcon, widget },
   data: () => ({
-    info:[
-      {label:'Feels Like',key:'feels_like',temp:true},
-      {label:'Humidity',key:'humidity',percent:true},
-      {label:'Sunrise',key:'sunrise',time:true},
-      {label:'Sunset',key:'sunset',time:true}
-    ]
+    info: [
+      { label: "Feels Like", key: "feels_like", temp: true },
+      { label: "Humidity", key: "humidity", percent: true },
+      { label: "Sunrise", key: "sunrise", time: true },
+      { label: "Sunset", key: "sunset", time: true },
+    ],
   }),
-  computed:{
-    ...mapState(['eventsWeather']),
-    data(){
-      return this.eventsWeather[this.uid] || {}
+  computed: {
+    ...mapState(["eventsWeather"]),
+    data() {
+      return this.eventsWeather[this.uid] || {};
     },
-    current(){
-      return this.data.current || null
-    }
   },
-  methods:{
-    getTemp(k){
-      return `${Math.round((k - 273.15) * 9/5 + 32)}°F`
+  methods: {
+    getTemp(k) {
+      return `${Math.round(((k - 273.15) * 9) / 5 + 32)}°F`;
     },
-    getTime(t){
-      let o = this.data.timezone_offset || 0
-      return new Date( t * 1000 + o * 1000).toLocaleTimeString('en-US',{
-        timeZone: 'UTC',
-        hour: '2-digit',
-        minute: '2-digit'
-      })
-    }
-  }
-}
+    getTime(t) {
+      let o = this.data.timezone_offset || 0;
+      return new Date(t * 1000 + o * 1000).toLocaleTimeString("en-US", {
+        timeZone: "UTC",
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-.c-weather-widget{
-
-  .c-widget-content{
+.c-weather-widget {
+  .c-widget-content {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
   }
 
-  .c-icon--wrapper{
+  .c-icon--wrapper {
     flex: 0 0 auto;
-    svg{
+    svg {
       display: block;
       width: 7rem;
     }
   }
 
-  .c-temp--wrapper{
+  .c-temp--wrapper {
     flex: 0 0 auto;
     text-align: center;
 
-    .c-temp-value{
+    .c-temp-value {
       font-size: 5rem;
     }
   }
 
-  .c-bar{
+  .c-bar {
     flex: 0 0 1px;
     align-self: stretch;
     background: $pink;
   }
 
-  .c-info--wrapper{
+  .c-info--wrapper {
     flex: 0 0 auto;
 
-    .c-info-table{
+    .c-info-table {
       text-align: left;
       font-weight: 800;
       font-size: 1.4rem;
     }
-    .c-info-label{
+    .c-info-label {
       text-align: right;
       padding-right: 1.5rem;
       font-weight: 400;
-      opacity: .75;
+      opacity: 0.75;
     }
   }
 }
