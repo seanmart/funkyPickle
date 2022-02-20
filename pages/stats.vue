67d8f34bb52a4766dd7d5aae8593002e837b0e81
Sean Metcalf <sean@seanmart.com>
2022-02-20 15:07:33 -0500

    added stats page

---
+++ pages/stats.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+  <div id="page">
+    <container>
+      <table>
+        <tbody>
+          <template v-for="item in data">
+            <tr class="border-b border-gray">
+              <td class="font-bold py-10 pr-20" v-html="`${item.label}:`"/>
+              <td class="py-10" v-html="item.value"/>
+            </tr>
+          </template>
+        </tbody>
+      </table>
+    </container>
+  </div>
+</template>
+
+<script>
+export default {
+  computed:{
+    data(){
+      if (process.server) return null
+      return [
+        {label: 'Browser',value: navigator.userAgent},
+        {label: 'maxTouchPoints',value: navigator.maxTouchPoints},
+        {label: 'Platform', value: navigator.platform}
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
