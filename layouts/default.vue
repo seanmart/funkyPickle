2e0a5cb00b2daa19970dbc5808437fe7fdfff049
Sean Metcalf <sean@seanmart.com>
2022-03-05 15:42:17 -0500

    bug tweaks

--- layouts/default.vue
+++ layouts/default.vue
@@ -22,12 +22,14 @@ export default {
   },
   mounted(){
     this.$bus.$emit('LOADED')
+    this.$bus.$on('REVEAL',()=> ScrollTrigger.refresh())
   },
   watch:{
     $route(){
       this.$refs.scroller.scrollTo(0,0)
       ScrollTrigger.getAll().length > 0 && ScrollTrigger.refresh(true)
-      gsap.timeline({onComplete:()=>this.$bus.$emit('REVEAL')})
+      gsap.timeline({delay:.25,onComplete:()=>this.$bus.$emit('REVEAL')})
+          .set('#scroller',{scale:.95})
           .to('#background',.75,{scale:1,ease:'expo.inOut'})
           .to('#background .bg',.75,{opacity:0,ease:'expo.inOut'},'<')
           .to('#background .strip',.75,{opacity:.05,ease:'expo.inOut',fill:config.theme.colors.blue},'<')
@@ -39,19 +41,21 @@ export default {
   middleware({from,route,$bus}){
     if (process.server || !from || from.path == route.path) return;
     let colors = [
+      config.theme.colors.lime,
       config.theme.colors.pink,
       config.theme.colors.green,
       config.theme.colors.black
     ]
     return new Promise(res =>{
       $bus.$emit('TOP_NAV',true)
-      let bg = config.theme.colors.lime
+      let bg = colors.splice(random(0,colors.length -1),1)
       gsap.timeline({onComplete:res})
           .set('#background .bg',{background:bg},0)
-          .to('#scroller',.75,{scale:.9,opacity:0,ease:'expo.inOut'},'<')
-          .to('#background',.75,{scale:1.1,ease:'expo.inOut'},'<')
+          .to('#scroller',.75,{scale:.95,opacity:0,ease:'expo.inOut'},'<')
+          .to('#background',.75,{scale:1.05,ease:'expo.inOut'},'<')
           .to('#background .bg',.75,{opacity:1,ease:'expo.inOut'},'<')
           .to('#background .strip',.75,{opacity:1,ease:'expo.inOut',fill:()=> colors[random(0,colors.length - 1)]},'<')
+          .set('#scroller',{scale:1})
     })
   },
   methods:{
