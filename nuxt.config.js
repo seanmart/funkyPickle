e5f2ac03eb71ea3edafe07ce3712821ae78e74fa
Sean Metcalf <sean@seanmart.com>
2022-03-08 16:57:27 -0500

    testing prismic preview

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,8 @@ export default {
       { src: "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.0/gsap.min.js" },
       { src: "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.0/ScrollTrigger.min.js" },
       { src: "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/ScrollToPlugin.min.js" },
-      {src:"https://unpkg.com/swiper@8/swiper-bundle.min.js"}
+      {src:"https://unpkg.com/swiper@8/swiper-bundle.min.js"},
+      {async:true, defer:true, src:"https://static.cdn.prismic.io/prismic.js?new=true&repo=funkypickle"}
     ],
   },
   css: ['@/assets/main.css'],
