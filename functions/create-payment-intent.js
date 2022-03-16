e1af7b3a24265104a45dc8596a8e5b1f94592a39
Sean Metcalf <sean@seanmart.com>
2022-03-16 01:53:15 -0400

    commenting out netlify function

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -1,42 +1,42 @@
-const axios = require("axios");
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY),
-  headers = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "Content-Type",
-  };
-
-exports.handler = async (event, context) => {
-  // CORS
-  if (event.httpMethod === "OPTIONS") {
-    return {
-      statusCode: 200,
-      headers,
-    };
-  }
-
-  try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      currency: "usd",
-      amount: 50,
-      description: "Register For Event",
-    });
-
-    return {
-      statusCode: 200,
-      headers,
-      body: JSON.stringify({
-        clientSecret: paymentIntent.client_secret,
-      }),
-    };
-  } catch (err) {
-    console.log(err);
-
-    return {
-      statusCode: 400,
-      headers,
-      body: JSON.stringify({
-        status: err,
-      }),
-    };
-  }
-};
+// const axios = require("axios");
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY),
+//   headers = {
+//     "Access-Control-Allow-Origin": "*",
+//     "Access-Control-Allow-Headers": "Content-Type",
+//   };
+//
+// exports.handler = async (event, context) => {
+//   // CORS
+//   if (event.httpMethod === "OPTIONS") {
+//     return {
+//       statusCode: 200,
+//       headers,
+//     };
+//   }
+//
+//   try {
+//     const paymentIntent = await stripe.paymentIntents.create({
+//       currency: "usd",
+//       amount: 50,
+//       description: "Register For Event",
+//     });
+//
+//     return {
+//       statusCode: 200,
+//       headers,
+//       body: JSON.stringify({
+//         clientSecret: paymentIntent.client_secret,
+//       }),
+//     };
+//   } catch (err) {
+//     console.log(err);
+//
+//     return {
+//       statusCode: 400,
+//       headers,
+//       body: JSON.stringify({
+//         status: err,
+//       }),
+//     };
+//   }
+// };
